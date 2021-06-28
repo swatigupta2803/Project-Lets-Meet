ce528ae32f22b7467927f1feafa0aaf6b0e6ee64
swatigupta2803 <65402557+swatigupta2803@users.noreply.github.com>
2021-06-28 19:46:32 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function required() {
+  var user = document.form1.text1.value;
+  var pass = document.form1.text2.value;
+
+
+    if(user =="" || pass =="") {
+    return false;
+  }
+  document.form1.submit;
+  return true;
+
+}
+  
